e4c77e78ead3096df13aa77d776cce3345adbfa0
Abhishek kumar <abhi.arya97161@gmail.com>
2024-06-15 14:20:36 +0530

    delete template bug fixed

--- controllers/Templete/deleteTemplate.js
+++ controllers/Templete/deleteTemplate.js
@@ -1,4 +1,5 @@
 const Template = require("../../models/TempleteModel/templete");
+const AssignData = require("../../models/TempleteModel/assigndata");
 
 const deleteTemplate = async (req, res) => {
   try {
@@ -8,16 +9,26 @@ const deleteTemplate = async (req, res) => {
     const template = await Template.findByPk(templateId);
 
     if (!template) {
-      return res.status(404).json({ error: 'Template not found' });
+      return res.status(404).json({ error: "Template not found" });
+    }
+
+    const assignData = await AssignData.findOne({
+      where: { templeteId: templateId },
+    });
+
+    if(assignData ){
+      return res.status(400).json({ error: "Template has assigned task" });
     }
 
     // Delete the template and cascade the deletion to related data
     await template.destroy();
 
-    res.status(200).json({ message: 'Template Deleted successfully' });
+    res.status(200).json({ message: "Template Deleted successfully" });
   } catch (error) {
-    console.error('Error deleting template:', error);
-    res.status(500).json({ error: 'An error occurred while deleting the template' });
+    console.error("Error deleting template:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the template" });
   }
 };
 
